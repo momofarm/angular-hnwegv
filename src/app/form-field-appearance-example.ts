0cb565d586e01f451b06dfd586b7e9e57b4d357b
momofarm <momo1x3@gmail.com>
2021-07-02 14:09:54 +0800

    add form signal slot

--- src/app/form-field-appearance-example.ts
+++ src/app/form-field-appearance-example.ts
@@ -1,12 +1,19 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 /** @title Form field appearance variants */
 @Component({
   selector: 'form-field-appearance-example',
-  templateUrl: 'form-field-appearance-example.html',
+  templateUrl: 'form-field-appearance-example.html'
 })
-export class FormFieldAppearanceExample {}
+export class FormFieldAppearanceExample {
+  @Output() eventAddItem = new EventEmitter<string[]>();
 
+  addItem(weight: string, length: string, width: string, height: string) {
+    console.log('form field add item pressed');
+    let temp: string[] = [weight, length, width, height];
+    this.eventAddItem.emit(temp);
+  }
+}
 
 /**  Copyright 2021 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
